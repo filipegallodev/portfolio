d171c5ba13f21a13700088e3119cb13809198c21
Filipe <filipegallodev@hotmail.com>
2024-08-12 21:44:31 -0300

    added profile picture in header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import HeaderNavigation from "./HeaderNavigation";
 import Link from "next/link";
+import ProfilePicture from "/public/assets/profile-picture.webp";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <HeaderContainer>
       <Container>
-        <DeveloperName>
-          <Link href="">Filipe Gallo</Link>
-        </DeveloperName>
+        <Link href="">
+          <DeveloperInfoContainer>
+            <ImageStyled src={ProfilePicture} alt="Foto de Filipe Gallo" />
+            <DeveloperName>Filipe Gallo</DeveloperName>
+          </DeveloperInfoContainer>
+        </Link>
         <HeaderNavigation />
       </Container>
     </HeaderContainer>
@@ -22,12 +27,24 @@ const HeaderContainer = styled.header`
   width: 100%;
   padding: 8px 64px;
   box-shadow: 0px 0px 48px 0px #00000015;
+  letter-spacing: 1px;
 `;
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+const DeveloperInfoContainer = styled(Container)`
+  gap: 16px;
+`;
+
+const ImageStyled = styled(Image)`
+  width: 64px;
+  height: auto;
+  border-radius: 40%;
+  filter: drop-shadow(0 0 0.75rem var(--main-color));
 `;
 
 const DeveloperName = styled.span`
